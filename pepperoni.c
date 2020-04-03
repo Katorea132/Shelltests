9e182e8727de3aac8ea1a941f218ba01edfd0be2
Katorea132 <katorea132@gmail.com>
2020-04-03 17:03:18 -0500

    Added pseudo-working version

--- pepperoni.c
+++ pepperoni.c
@@ -16,14 +16,12 @@ int main(void)
 	if (!p)
 	{
 		megaenv = malloc(sizeof(char *) * (i + 2));
-		for (j = 0; j < i + 1; j++)
-			megaenv[j] = strdup(environ[j]);
-		megaenv[j] = malloc(sizeof(char) * ((strlen(entry) + (strlen(val) + 2))));
-		/*megaenv[j] = entry, megaenv[j] = strcat(megaenv[j], "=");
-		megaenv[j] = strcat(megaenv[j], val);*/
+		memcpy(megaenv, environ, (i + 2) * sizeof(char *));
 	}
 	for (i = 0; megaenv[i]; i++)
 		printf("%s\n", megaenv[i]);
+	megaenv[i] = "Pizza=aguacate";
+	printf("%s\n", megaenv[i]);
 
 	return (0);
 }
