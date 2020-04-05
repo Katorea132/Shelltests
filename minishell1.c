dff97d9ff25d3bbefbc62ee0f390c6d045d9efb7
Katorea132 <katorea132@gmail.com>
2020-04-05 12:30:55 -0500

    Added new experimental version of minishell

--- minishell1.c
+++ minishell1.c
@@ -4,46 +4,51 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <sys/stat.h>
 char **command (char *buf)
 {
-	char *tmp1, *tmp2, *token, **arr;
+	char *tmp1 = 0, *tmp2 = 0, *token = 0, **arr = 0;
 	int i;
 
 	tmp1 = strdup(buf);
 	tmp2 = strdup(buf);
-	for (i = 0, token = strtok(tmp1, " \n"); token; i++, token = strtok(NULL, " \n"))
-		;
+	for (i = 0, token = strtok(tmp1, " \n"); token; i++)
+		token = strtok(NULL, " \n");
 	free(tmp1);
 	arr = malloc(sizeof(char *) * i);
-	for (i = 0, token = strtok(tmp2, " \n"); token; i++, token = strtok(NULL, " \n"))
-		arr[i] = strdup(token);
+	for (i = 0, token = strtok(tmp2, " \n"); token; i++)
+		arr[i] = strdup(token), token = strtok(NULL, " \n");
+	arr[i] = NULL;
 	free(tmp2);
 	return (arr);	
 }
 int main (int ac __attribute__((unused)), char **argv)
 {
-	char *buffer = 0, **arr;
-	unsigned long int len, i;
-	pid_t piddy;
+	char *buffer = 0, **arr = 0;
+	unsigned long int len = 0, i = 0;
+	int getty = 1;
+	pid_t piddy = 0;
+	struct stat buf;
 
-	for (; ;)
+	while (getty != -1)
 	{
-		printf("%s$ ", argv[0]);
-		getline(&buffer, &len, stdin);
+		write(1, argv[0], strlen(argv[0]));
+		write(1, "$ ", 2);
+		getty = getline(&buffer, &len, stdin);
 		arr = command(buffer);
-		piddy = fork();
-		if (piddy == 0)
+		if (stat(arr[0], &buf) == 0 && buf.st_mode & S_IXUSR)
 		{
-			execve(arr[0], arr, NULL);
+			piddy = fork();
+			if (piddy == 0)
+				execve(arr[0], arr, NULL);
+			else
+				wait(NULL);
 		}
-		else
-		{
-			wait(NULL);
-			for (i = 0; arr[i]; i++)
-				free (arr[i]);
-			free(buffer);
-			buffer = NULL;
-		}
+		for (i = 0; arr[i]; i++)
+			free (arr[i]);
+		free(arr);
+		free(buffer);
+		buffer = NULL;
 		
 	}
 	return (0);
