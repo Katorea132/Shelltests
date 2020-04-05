10df09e989985ebf90113c639e37863c78b925d7
valvarezgi <vale.agiraldo@gmail.com>
2020-04-05 16:01:37 -0500

    Comments corrected

--- resources/shellV.h
+++ resources/shellV.h
@@ -10,19 +10,19 @@
 #include <sys/stat.h>
 
 extern char **environ;
-/* These are inside stringS. */
+/* These are inside stringsV. */
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
-/* These are inside stringS1. */
+/* These are inside stringsV1. */
 char *_strdup(char *str);
 int _strcmp(char *s1, char *s2);
 char *_strchr(char *s, char c);
 char *_strncat(char *dest, char *src, int n);
 char *_strpbrk(char *s, char *accept);
-/* These are inside utilitiesS.c */
+/* These are inside utilitiesV.c */
 char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* of SHELLY */
